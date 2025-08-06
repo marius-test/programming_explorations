53d4ada9d518b7b56f0d8edb7150ee0224d57c26
Marius Briscut <marius.briscut5@gmail.com>
2025-08-06 07:16:36 -0400

    wallpaper modification

--- python/ascii_image_generator.py
+++ python/ascii_image_generator.py
@@ -1,12 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
 import pyfiglet
 
-ascii_art = pyfiglet.figlet_format("NASA")
+ascii_art = pyfiglet.figlet_format("Melanie")
 
 img_width = 1080
 img_height = 1920
 
-img = Image.new("RGB", (img_width, img_height), color="black")
+img = Image.new("RGB", (img_width, img_height), color=(24, 62, 125))
 draw = ImageDraw.Draw(img)
 
 # load font
@@ -22,7 +22,7 @@ y_text = (img_height - total_text_height) // 2
 for line in lines:
     line_width = font.getbbox(line)[2] - font.getbbox(line)[0]  # width from bounding box
     x_text = (img_width - line_width) // 2
-    draw.text((x_text, y_text), line, font=font, fill="blue")
+    draw.text((x_text, y_text), line, font=font, fill=(249, 249, 249))
     y_text += line_height
 
-img.save("nasa_ascii_wallpaper.png")
+img.save("ascii_vertical_wallpaper.png")
